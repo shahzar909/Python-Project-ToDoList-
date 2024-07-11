0f8dea6d065abc00d9f099cc6cd03081ece7c818
Shahzar Iqbal <shahzarkhan909@gmail.com>
2024-07-11 17:52:08 +0530

    rename

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,31 +1,30 @@
 import random
 
-def play_game():
+user_score = 0
+computer_score = 0
+
+while True:
     user_choice = int(input("Enter your Choice: Type 0 for Rock, 1 for Paper, 2 for Scissors: "))
-    if user_choice < 0 or user_choice > 2:
+    
+    if user_choice < 0 or user_choice >= 3:
         print("Invalid Choice, You Lose... Try Again!!")
     else:
         computer_choice = random.randint(0, 2)
-        print("Computer Chose:", computer_choice)
-        
+        print(f"Computer Chose: {computer_choice}")
+
         if computer_choice == user_choice:
             print("It's a Draw.")
-        elif computer_choice == 0 and user_choice == 2:
+        elif (computer_choice == 0 and user_choice == 2) or (computer_choice == 1 and user_choice == 0) or (computer_choice == 2 and user_choice == 1):
             print("You Lose.")
-        elif user_choice == 0 and computer_choice == 2:
+            computer_score += 1
+        else:
             print("You Win.")
-        elif computer_choice > user_choice:
-            print("You Lose.")
-        elif user_choice > computer_choice:
-            print("You Win.")
+            user_score += 1
 
-if __name__ == "__main__":
-    while True:
-        play_game()
-        continue_choice = input("Do you want to play again? (yes/no): ").strip().lower()
-        if continue_choice != "yes":
-            print("Goodbye!")
-            break
+        print(f"Current Scores - You: {user_score}, Computer: {computer_score}")
 
-
-
+    continue_choice = input("\nDo you want to play another round? (yes/no): ").strip().lower()
+    if continue_choice != "yes":
+        print("Final Scores - You: {user_score}, Computer: {computer_score}")
+        print("Goodbye!")
+        break
